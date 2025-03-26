0f4819d01a6f3a2ade4ab9e0f11d054b7e16180e
Geo <gjsj@discente.ifpe.edu.br>
2025-03-26 20:50:16 -0300

    finished home page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { BottonCvComponent } from './components/botton-cv/botton-cv.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfileImgComponent } from './components/profile-img/profile-img.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleComponent,
+    BottonCvComponent,
+    FooterComponent,
+    ProfileImgComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/botton-cv/botton-cv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botton-cv',
+  templateUrl: './botton-cv.component.html',
+  styleUrls: ['./botton-cv.component.css']
+})
+export class BottonCvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
